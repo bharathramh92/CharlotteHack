a8f736d33d73fc6962fdc0e8561c8e352810dc9a
bharath <bharathramh92@gmail.com>
2016-01-30 14:09:13 -0500

    bug fixes

--- search/views.py
+++ search/views.py
@@ -6,9 +6,11 @@ from search.models import Hospital
 
 
 def rank(zipcode, hosp_type, emergency, criteria):
+# def rank():
     data = Hospital.objects.all()
     keys = "MORT_30_AMI,MORT_30_HF,MORT_30_PN,PSI_90_SAFETY,IMM_2,PN_6,SCIP_CARD_2,SCIP_INF_2,SCIP_INF_3,SCIP_INF_9,SCIP_VTE_2,AMI_7A,HAI1,HAI2,HAI3,HAI4,H_CLEAN_HSP_A_P,H_COMP_1_A_P,H_COMP_2_A_P,H_COMP_3_A_P,H_COMP_4_A_P,H_COMP_5_A_P,H_COMP_7_A,H_COMP_7_SA,H_QUIET_HSP_A_P,H_CLEAN_QUIET,H_COMP_7,MSPB_1"
     keys_list = keys.split(",")
+
     def weight(dt):
         x = 0
         dt = dt.__dict__
@@ -28,6 +30,7 @@ def index(request):
         form = SearchForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
+            print("here")
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
@@ -35,13 +38,15 @@ def index(request):
             hosp_type = form.cleaned_data["hosp_type"]
             emergency = form.cleaned_data["emergency"]
             criteria = form.cleaned_data["criteria"]
-
+            print(form.errors)
             data = rank(zipcode, hosp_type, emergency, criteria)
 
             return render(request, "search/result.html", {'form': form, 'data': data})
-
+        # else:
+        #     print("invalid form")
+        #     print(form.errors)
     # if a GET (or any other method) we'll create a blank form
     else:
         form = SearchForm()
 
-    return render(request, "search/index.html", {'form': form})
+    return render(request, "search/index_.html", {'form': form})
